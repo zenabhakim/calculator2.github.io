95a4eb86e5319dc46f17c3640f5955fbce3549fc
zenabhakim <134938083+zenabhakim@users.noreply.github.com>
2023-06-21 18:43:13 +0530

    Add files via upload
    commited some changes

---
+++ index.js
@@ -0,0 +1,11 @@
+function valueButton(e){
+  calculatorform.screen.value += e.value
+}
+
+function Clear(){
+  calculatorform.screen.value= null;
+}
+
+function Calculate(){
+  calculatorform.screen.value = eval(calculatorform.screen.value)
+}
